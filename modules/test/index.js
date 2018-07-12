d3c9ad56cfd432e962d1710dfa9bc84f574e0268
BorisKolyarov <17835383+BorisKotlyarov@users.noreply.github.com>
2018-07-12 17:38:38 +0300

    New example has been added

---
+++ modules/test/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+    //Example get request with RegExp path
+    GET: {
+        '/test/([0-9]{1,})': function(request, response, matched){
+            //use http://localhost:3002/test/123
+            response.resp(matched);
+        }
+    },
+
+    //Example post request
+    POST : {
+        '/test': async function(request, response){
+            let body = await request.body;
+            response.resp(body);
+        }
+    }
+};
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const {Modules, Server} = require('just-rest');
 
 Modules.define(__dirname + '/modules/main/index.js');
+Modules.define(__dirname + '/modules/test/index.js');
 
 new Server({Modules, port: 3002 });
