59f547cce36e7d62c6c7daf6b797c6d9827fee2f
BorisKolyarov <17835383+BorisKotlyarov@users.noreply.github.com>
2018-07-14 11:52:48 +0300

    corsAllowHeaders interceptor has been fixed

--- interceptors/response/corsAllowHeaders.js
+++ interceptors/response/corsAllowHeaders.js
@@ -1,16 +1,44 @@
+function corsAllowHeaders(response){
+    const CorsAllowHeaders = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Set-Cookies, Access-Token'
+    };
+
+    Object.keys(CorsAllowHeaders).forEach((item) => {
+        response.setHeader(item, CorsAllowHeaders[item]);
+    });
+}
+
 module.exports = {
+
     GET: {
         '/(.+?)': function(response){
+            corsAllowHeaders(response);
+        }
+    },
 
-            const CorsAllowHeaders = {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-                'Access-Control-Allow-Headers': 'Content-Type, Set-Cookies, Access-Token'
-            };
+    POST: {
+        '/(.+?)': function(response){
+            corsAllowHeaders(response);
+        }
+    },
+    PUT: {
+        '/(.+?)': function(response){
+            corsAllowHeaders(response);
+        }
+    },
 
-            Object.keys(CorsAllowHeaders).forEach((item) => {
-                response.setHeader(item, CorsAllowHeaders[item]);
-            });
+    DELETE: {
+        '/(.+?)': function(response){
+            corsAllowHeaders(response);
+        }
+    },
+
+    OPTIONS: {
+        '/(.+?)': function(response){
+            corsAllowHeaders(response);
         }
     }
+
 };
