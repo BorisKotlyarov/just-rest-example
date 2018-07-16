55c57207d77455677a54485f53eba8bc45f9d17e
BorisKolyarov <17835383+BorisKotlyarov@users.noreply.github.com>
2018-07-16 15:55:39 +0300

    support version 0.0.8-alpha has been added

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const {Modules, Server} = require('just-rest');
 
-Modules.defineRequestInterceptor(__dirname + '/interceptors/request/cookies.js');
-Modules.defineResponseInterceptor(__dirname + '/interceptors/response/corsAllowHeaders.js');
-Modules.define(__dirname + '/modules/main/index.js');
-Modules.define(__dirname + '/modules/test/index.js');
+Modules.defineRequestInterceptor('just-rest-cookies');
+Modules.defineResponseInterceptor('./interceptors/response/corsAllowHeaders.js');
+Modules.define('./modules/main/index.js');
+Modules.define('./modules/test/index.js');
 
 new Server({Modules, port: process.env.PORT || 3002 });
