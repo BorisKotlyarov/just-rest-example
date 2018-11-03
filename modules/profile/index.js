ed2533c5da3b09e137af57c71995a44c886ab7a4
BorisKolyarov <17835383+BorisKotlyarov@users.noreply.github.com>
2018-11-03 15:15:04 +0200

    Migrate to «just-rest» version 0.0.13-alpha has been added

--- modules/profile/index.js
+++ modules/profile/index.js
@@ -1,19 +1,67 @@
+const {Errors} = require('just-rest');
 
+function user(request, response, match){
+    let instance = this;
+
+    //TODO get data from real database
+
+    instance.user = {
+        username: 'Guest',
+        permissions: [],
+        isAuthorized: false
+    };
+
+    let testDatabase = {
+        'token1': {
+            username: 'Boris',
+            permissions: ['all'],
+            isAuthorized: true
+        },
+        'token2': {
+            username: 'User 2',
+            permissions: ['read.me'],
+            isAuthorized: true
+        },
+        'token3': {
+            username: 'User 3',
+            permissions: ['read.something'],
+            isAuthorized: true
+        },
+    }
+
+    if (request.headers.token && testDatabase.hasOwnProperty(request.headers.token)) {
+        instance.user = testDatabase[request.headers.token];
+    }
+
+    return;
+}
+
+function isAuthorized() {
+    if (!this.user.isAuthorized) {
+        throw  new Errors(401)
+    }
+    return;
+}
+
+function readPermission() {
+    if (!this.user.permissions.includes('read.me') && !this.user.permissions.includes('all')) {
+        throw  new Errors(403)
+    }
+    return;
+}
+
+function controller(request, response, matched) {
+    response.resp(this.user);
+}
 
 module.exports = {
-
-    //Example get request with RegExp path
     GET: {
-        '/profile/me': function (request, response, matched) {
-            //use http://localhost:3002/profile/me
-
-            /*
-                Using middleware.
-                Make middleware see file "{__PROJECT_DIR__}/interceptors/request/auth.js"
-            */
-            this.checkAuth();
-
-            response.resp(this.user);
-        }
+        //use http://localhost:3002/profile/me
+        '/profile/me': [
+            user, // Using middleware. add user variable to instance
+            isAuthorized, // Using middleware. check authorize
+            readPermission, // Using middleware. check read permissions
+            controller
+        ]
     }
 };
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const {Modules, Server} = require('just-rest');
 
 Modules.defineRequestInterceptor('just-rest-cookies');
 Modules.defineResponseInterceptor('./interceptors/response/corsAllowHeaders.js');
-Modules.defineRequestInterceptor('./interceptors/request/auth.js');
+
 Modules.define('./modules/main/index.js');
 Modules.define('./modules/test/index.js');
 Modules.define('./modules/profile/index.js');
